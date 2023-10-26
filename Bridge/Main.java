b231c089d3216889ff3fbd462ee040ae953dec62
SangNguyen1407 <50044154+SangNguyen1407@users.noreply.github.com>
2023-10-26 22:30:04 +0900

    add bridge

---
+++ Bridge/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Shape square = new Square();
+
+        System.out.println(square.draw());
+    }
+}
---
+++ Bridge/Shape.java
@@ -0,0 +1,5 @@
+public abstract class Shape { 
+    //standard constructors
+    
+    abstract public String draw();
+}
